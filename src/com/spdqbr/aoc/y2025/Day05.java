3c3473c7033684220319ea43f435e2b8917c7c30
Daniel Hayes <spdqbr@gmail.com>
2025-12-05 00:58:24 -0700

    2025 Day 05

--- src/com/spdqbr/aoc/y2025/Day05.java
+++ src/com/spdqbr/aoc/y2025/Day05.java
@@ -1,7 +1,12 @@
 package com.spdqbr.aoc.y2025;
-import static com.spdqbr.aoc.utils.Utils.*;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import com.spdqbr.aoc.Solution;
+import com.spdqbr.aoc.utils.BigRange;
 
 public class Day05 extends Solution {
 	public static final int day = 5;
@@ -10,16 +15,65 @@ public class Day05 extends Solution {
 	public static void main(String[] args) {
 //		DEBUG = true;
 		(new Day05()).main(year, day, 0);
-//		(new Day05()).main(year, day, 1);
-//		(new Day05()).main(year, day, 2);
-//		(new Day05()).main(year, day, 3);
+		(new Day05()).main(year, day, 1);
+		(new Day05()).main(year, day, 2);
+		(new Day05()).main(year, day, 3);
 	}
 	
 	public String part1(String[] input) {
-		return "";
+		List<BigRange> ranges = new ArrayList<>();
+		String[] val;
+		BigInteger n;
+		long count = 0;
+		for(String s : input) {
+			if(s.contains("-")){
+				val = s.split("-");
+				ranges.add(new BigRange(new BigInteger(val[0]), new BigInteger(val[1])));
+			}else if (! s.isBlank()) {
+				n = new BigInteger(s);
+				for(BigRange r : ranges) {
+					if(r.contains(new BigRange(n,n))) { 
+						count++;
+						break;
+					}
+				}
+			}
+		}
+		return ""+count;
+	}
+	
+	public static BigInteger max(BigInteger a, BigInteger b) {
+		return a.compareTo(b) > 0 ? a : b;
 	}
 	
 	public String part2(String[] input) {
-		return "";
+		List<BigRange> ranges = new ArrayList<>();
+		String[] val;
+		for(String s : input) {
+			if(s.contains("-")){
+				val = s.split("-");
+				ranges.add(new BigRange(new BigInteger(val[0]), new BigInteger(val[1])));
+			}else if (! s.isBlank()) {
+				break;
+			}
+		}
+		
+		Collections.sort(ranges, new Comparator<BigRange>() {
+			@Override
+			public int compare(BigRange o1, BigRange o2) {
+				return o1.left.compareTo(o2.left);
+			}
+		});
+		
+		BigInteger maxRight = BigInteger.ZERO;
+		BigInteger sum = BigInteger.ZERO;
+		
+		BigInteger left;
+		for(BigRange range : ranges) {
+			left = max(range.left, maxRight.add(BigInteger.ONE));
+			maxRight = max(range.right, maxRight);
+			sum = sum.add(max(BigInteger.ZERO, maxRight.subtract(left).add(BigInteger.ONE)));
+		}
+		return ""+sum;
 	}
 }
