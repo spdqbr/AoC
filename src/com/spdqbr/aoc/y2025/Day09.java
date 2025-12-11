277bf9a7e35088b1b0f52bffd8603d3305e7ab1e
Daniel Hayes <spdqbr@gmail.com>
2025-12-10 22:55:04 -0700

    2025 Days 09 and 11 (Java). Day 10 is WIP

--- src/com/spdqbr/aoc/y2025/Day09.java
+++ src/com/spdqbr/aoc/y2025/Day09.java
@@ -1,7 +1,12 @@
 package com.spdqbr.aoc.y2025;
-import static com.spdqbr.aoc.utils.Utils.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import com.spdqbr.aoc.Solution;
+import com.spdqbr.aoc.utils.Pair;
 
 public class Day09 extends Solution {
 	public static final int day = 9;
@@ -10,16 +15,117 @@ public class Day09 extends Solution {
 	public static void main(String[] args) {
 //		DEBUG = true;
 		(new Day09()).main(year, day, 0);
-//		(new Day09()).main(year, day, 1);
-//		(new Day09()).main(year, day, 2);
-//		(new Day09()).main(year, day, 3);
+		(new Day09()).main(year, day, 1);
+		(new Day09()).main(year, day, 2);
+		(new Day09()).main(year, day, 3);
 	}
 	
 	public String part1(String[] input) {
-		return "";
+		List<Long[]> grid = new ArrayList<>();
+		
+		String[] vals;
+		for(String s : input) {
+			vals = s.split(",");
+			grid.add(new Long[] { Long.parseLong(vals[0]), Long.parseLong(vals[1]) });
+		}
+		
+		long maxArea = 0;
+		long area;
+		Long[] a, b;
+		for(int i = 0; i < grid.size(); i++) {
+			a = grid.get(i);
+			for(int j = i+1; j < grid.size(); j++) {
+				b = grid.get(j);
+				area = Math.abs(a[0] - b[0]+1) * Math.abs(a[1] - b[1]+1);
+				debug(Arrays.toString(a)+" : "+Arrays.toString(b)+" :: "+area);
+				if(area > maxArea) {
+					maxArea = area;
+				}
+			}
+		}
+		
+		return ""+maxArea;
 	}
 	
 	public String part2(String[] input) {
-		return "";
+		List<int[]> grid = new ArrayList<>();
+		
+		String[] vals;
+		int row, col;
+		for(String s : input) {
+			vals = s.split(",");
+			row = Integer.parseInt(vals[0]);
+			col = Integer.parseInt(vals[1]);
+			grid.add(new int[] { row, col });
+		}
+		
+		Set<Pair<Integer, Integer>> perimeter = new HashSet<>();
+		int[] a, b;
+		int min, max;
+		for(int i = 0; i < grid.size(); i++) {
+			a = grid.get(i);
+			b = grid.get((i+1)%grid.size());
+			if(a[0] == b[0]) {
+				min = Math.min(a[1], b[1]);
+				max = Math.max(a[1], b[1]);
+				for(int j = min; j <= max; j++){
+					perimeter.add(new Pair<Integer, Integer>(a[0], j));
+				}
+			} else {
+				min = Math.min(a[0], b[0]);
+				max = Math.max(a[0], b[0]);
+				for(int j = min; j <= max; j++){
+					perimeter.add(new Pair<Integer, Integer>(j, a[1]));
+				}
+			}
+		}
+		
+		int minRow, maxRow, minCol, maxCol, area, maxArea = 0;
+		Pair<Integer, Integer> check = new Pair<Integer, Integer>(0,0);
+		for(int i = 0; i < grid.size(); i++) {
+			a = grid.get(i);
+			for(int j = i+1; j < grid.size(); j++) {
+				b = grid.get(j);
+				minRow = Math.min(a[0], b[0]);
+				maxRow = Math.max(a[0], b[0]);
+				minCol = Math.min(a[1], b[1]);
+				maxCol = Math.max(a[1], b[1]);
+				
+				area = (maxRow - minRow + 1) * (maxCol - minCol+1);
+				
+				if(area > maxArea) {
+					boolean isValid = true;
+					
+					// this whole block should be replaced with checking intersections rather than walking the whole path
+						// traverse the left and right sides of the rectangle, looking for perimeter crossings
+						for(int k = minRow+1; isValid && k < maxRow; k++) {
+							check.left = k;
+							
+							check.right = minCol+1;
+							if(perimeter.contains(check)) isValid = false;
+							
+							check.right = maxCol-1;
+							if(perimeter.contains(check)) isValid = false;
+						}
+						
+						// traverse the top and bottom sides of the rectangle, looking for perimeter crossings
+						for(int k = minCol+1; isValid && k < maxCol; k++) {
+							check.right = k;
+							
+							check.left = minRow+1;
+							if(perimeter.contains(check)) isValid = false;
+							
+							check.left = maxRow-1;
+							if(perimeter.contains(check)) isValid = false;
+						}
+					
+					if(isValid) {
+						maxArea = area;
+					}
+				}
+			}
+		}
+		
+		return ""+maxArea;
 	}
 }
--- src/com/spdqbr/aoc/y2025/Day11.java
+++ src/com/spdqbr/aoc/y2025/Day11.java
@@ -1,7 +1,9 @@
 package com.spdqbr.aoc.y2025;
-import static com.spdqbr.aoc.utils.Utils.*;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.spdqbr.aoc.Solution;
+import com.spdqbr.aoc.utils.Pair;
 
 public class Day11 extends Solution {
 	public static final int day = 11;
@@ -10,16 +12,77 @@ public class Day11 extends Solution {
 	public static void main(String[] args) {
 //		DEBUG = true;
 		(new Day11()).main(year, day, 0);
-//		(new Day11()).main(year, day, 1);
-//		(new Day11()).main(year, day, 2);
-//		(new Day11()).main(year, day, 3);
+		(new Day11()).main(year, day, 1);
+		(new Day11()).main(year, day, 2);
+		(new Day11()).main(year, day, 3);
+	}
+	
+	Map<String, Integer> memo = new HashMap<>();
+	public int countPaths(Map<String, String[]> paths, String node, String goal, String tab) {
+		String key = node+","+goal;
+		if(memo.containsKey(key)) return memo.get(key);
+		
+		if(node.equals(goal)) return 1;
+		
+		int count = 0;
+		for(String neighbor : paths.get(node)){
+			debug(tab + neighbor);
+			count += countPaths(paths, neighbor, goal, tab+" ");
+		}
+		
+		memo.put(key, count);
+		return count;
 	}
 	
 	public String part1(String[] input) {
-		return "";
+		memo.clear();
+		Map<String, String[]> paths = new HashMap<>();
+		String start = "you";
+		
+		String[] vals;
+		for(String s : input) {
+			vals = s.split(": ");
+			paths.put(vals[0], vals[1].split(" "));
+		}
+		
+		int count = countPaths(paths, start, "out", "");
+		
+		return ""+count;
+	}
+	
+	Map<String, Pair<Long, Boolean[]>> memoTrack = new HashMap<>();
+	public Long trackPaths(Map<String, String[]> paths, String node, String goal, String tab, boolean seen_fft, boolean seen_dac) {
+		String key = node+","+goal+","+seen_fft+","+seen_dac;
+		if(memoTrack.containsKey(key)) return memoTrack.get(key).left;
+		
+		if(node.equals(goal)) {
+			if(seen_fft && seen_dac) return 1L;
+			return 0L;
+		}
+		
+		long count = 0;
+		for(String neighbor : paths.get(node)){
+			debug(tab + neighbor+","+seen_fft+","+seen_dac);
+			count += trackPaths(paths, neighbor, goal, tab+" ", seen_fft || neighbor.equals("fft"), seen_dac || neighbor.equals("dac"));
+		}
+		
+		memoTrack.put(key, new Pair<Long, Boolean[]>(count, new Boolean[] {seen_fft, seen_dac}));
+		return count;
 	}
 	
 	public String part2(String[] input) {
-		return "";
+		memoTrack.clear();
+		Map<String, String[]> paths = new HashMap<>();
+		String start = "svr";
+		
+		String[] vals;
+		for(String s : input) {
+			vals = s.split(": ");
+			paths.put(vals[0], vals[1].split(" "));
+		}
+		
+		long count = trackPaths(paths, start, "out", "", false, false);
+		
+		return ""+count;
 	}
 }
